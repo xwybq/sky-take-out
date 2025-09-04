fb47f5b1a196c4ffea276c96635a868fe721434c
xwybq <3290819205@qq.com>
2025-09-04 11:33:12 +0800

    订单支付功能代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -13,6 +13,9 @@ public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User getByOpenid(@Param("openid") String openid);
 
-//    @AutoFill(value = OperationType.INSERT)
+    //    @AutoFill(value = OperationType.INSERT)
     void insert(User user);
+
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
